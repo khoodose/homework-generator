3f94fd5e2cf406e770d9791ca9d2ed7c0a2658bf
KT Khoo <khoodose@gmail.com>
2016-12-18 22:38:13 +0800

    add subtraction functionality and display

--- app.js
+++ app.js
@@ -9,20 +9,38 @@ $(function(){
   // populate the fields on page load
   generateAddition();
 
+  generateSubtraction();
+
   function generateAddition() {
-    var Answer = Math.floor(Math.random()*maximumValue + 1);
-    var N1 = Math.floor(Math.random()*Answer + 1);
-    var N2 = Answer - N1;
+    var additionAnswer = Math.floor(Math.random()*maximumValue + 1);
+    var A1 = Math.floor(Math.random()*additionAnswer + 1);
+    var A2 = additionAnswer - A1;
 
-    console.log("Answer = " + Answer + '\n' + "N1 = " + N1 + '\n' + "N2 = " + N2);
+    console.log("Addition Answer = " + additionAnswer + '\n' + "A1 = " + A1 + '\n' + "A2 = " + A2);
 
-    $("#N1").html(N1);
-    $("#N2").html(N2);
-    $("#Answer").html(Answer);
+    $("#A1").html(A1);
+    $("#A2").html(A2);
+    $("#answer_addition").html(additionAnswer);
+  }
+
+  function generateSubtraction() {
+    var S1 = Math.floor(Math.random()*maximumValue + 1);
+    var S2 = Math.floor(Math.random()*S1 + 1);
+    var subtractionAnswer = S1 - S2;
+
+    console.log("Subtraction Answer = " + subtractionAnswer + '\n' + "S1 = " + S1 + '\n' + "S2 = " + S2);
+
+    $("#S1").html(S1);
+    $("#S2").html(S2);
+    $("#answer_subtraction").html(subtractionAnswer);
   }
 
   $("#generateAdditionButton").click(function(){
     generateAddition()
+  });
+
+  $("#generateSubtractionButton").click(function(){
+    generateSubtraction()
   });
 
 });
